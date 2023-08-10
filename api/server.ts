cf45f730f1c33f49b1c59a5f6017b51c88892cc7
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-08-10 14:59:03 +0300

    Connect mongodb server

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,28 @@
 import express from 'express';
-const app = express();
+import { MongoClient } from 'mongodb';
+import dotenv from "dotenv";
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000")
+dotenv.config(); //load variables from .env file
+const app = express();
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const mongoURI: string | undefined = process.env.MONGOURI!;
+
+if (!mongoURI || !port) {
+    console.error('MongoDB URI or database name not found in env variable');
+    process.exit(1)
+}
+
+const connectDB = async () => {
+    try {
+        const client = new MongoClient(mongoURI);
+        await client.connect()
+        console.log('Connected to MongoDB database');
+
+    } catch (error) {
+        console.log('Error connecting to MongoDB', error)
+    }
+}
+app.listen(port, () => {
+    connectDB()
+    console.log(`Server listening on ${port}`)
 })
