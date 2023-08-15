7ea628717c7177d2201a160f4880d36294e471a7
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-08-15 03:23:38 +0300

    Add jwt and error middlewares

---
+++ api/middlewares/middlewares.ts
@@ -0,0 +1,41 @@
+// isLoggedInMiddleware.ts
+import { Request, Response, NextFunction } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: string; // Add any custom properties you need
+            userType?: string;
+        }
+    }
+}
+
+export const jwtMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ error: "Missing token" })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY!) as JwtPayload;
+        req.userId = decoded.userId;
+        req.userType = decoded.userType
+        next()
+    } catch (error) {
+        res.status(401).json({ error: 'Authentication failed: Invalid token' })
+    }
+}
+
+
+export const errorMiddleware = (error: any, req: Request, res: Response, next: NextFunction) => {
+    const { code, message } = error;
+    // Default Code
+    const statusCode = code || 500;
+
+    // Default Message
+    const errorMessage = message || error.message;
+
+    // Send response
+    res.status(statusCode).json({ error: errorMessage });
+}
